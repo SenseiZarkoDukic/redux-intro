d38f79b1acf1f2fcd748cc07e4a9586efb504d67
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-09 17:45:13 +0100

    Install react-redux so redux can connect and work with react

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { combineReducers, createStore } from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
+
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
