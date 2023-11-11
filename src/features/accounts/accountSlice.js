42c388b92cb781ae27de84e1b5b0c02657ffae00
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-11 12:52:30 +0100

    Condition in deposit action adding deposit in USD and else in other currencies with converter to USD as a middleware

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -34,7 +34,19 @@ export default function accountReducer(state = initialStateAccount, action) {
 }
 
 export function deposit(amount, currency) {
-  return { type: "account/deposit", payload: amount };
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    // API call to convert currency
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+    // return action
+
+    dispatch({ type: "account/deposit", payload: converted });
+  };
 }
 export function withdraw(amount) {
   return { type: "account/withdraw", payload: amount };
